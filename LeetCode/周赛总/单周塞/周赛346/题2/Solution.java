4946fb0e15451a2db154b7cbeb4c332126ad0528
Jackson_Wu <dengwu.wu@foxmail.com>
2023-05-21 16:35:11 +0800

    feat: 周赛346

---
+++ LeetCode/周赛总/单周塞/周赛346/题2/Solution.java
@@ -0,0 +1,27 @@
+package LeetCode.周赛总.单周塞.周赛346.题2;
+
+import java.util.Arrays;
+
+/**
+ * @author Wu
+ * @date 2023年05月21日 10:26
+ */
+public class Solution {
+    public String makeSmallestPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        char[] sb = s.toCharArray();
+        while (l <= r) {
+            if (s.charAt(l) == s.charAt(r)) {
+            } else {
+                if (s.charAt(l) < s.charAt(r)) {
+                    sb[r] = sb[l];
+                } else {
+                    sb[l] = sb[r];
+                }
+            }
+            l++;
+            r--;
+        }
+        return String.valueOf(sb);
+    }
+}
---
+++ LeetCode/周赛总/单周塞/周赛346/题4/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.周赛总.单周塞.周赛346.题4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Wu
+ * @date 2023年05月21日 10:26
+ */
+//TODO
+//https://leetcode.cn/contest/weekly-contest-346/problems/modify-graph-edge-weights/
+public class Solution {
+
+}
