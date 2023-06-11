458d8f7dcc795d2dc0367b86746aa6f9ece3a78c
Wu <dengwu.wu@foxmail.com>
2023-06-11 16:32:04 +0800

    feat: 周赛349

---
+++ LeetCode/周赛总/单周塞/周赛349/题1/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.周赛总.单周塞.周赛349.题1;
+
+import java.util.Arrays;
+
+/**
+ * @author Wu
+ * @date 2023年06月11日 19:31
+ */
+public class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if (nums.length == 2 || nums.length == 1) return -1;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[0] && nums[i] != nums[nums.length - 1]) return nums[i];
+        }
+        return -1;
+    }
+}
---
+++ LeetCode/周赛总/单周塞/周赛349/题4/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.周赛总.单周塞.周赛349.题4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Wu
+ * @date 2023年06月11日 19:31
+ */
+public class Solution {
+
+}
