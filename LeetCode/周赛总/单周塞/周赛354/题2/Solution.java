bda22d29f26213320a6441b5e6b8198b722fc77a
Jackson_Wu <dengwu.wu@foxmail.com>
2023-07-16 14:12:38 +0800

    feat: 周赛354

---
+++ LeetCode/周赛总/单周塞/周赛354/题2/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode.周赛总.单周塞.周赛354.题2;
+
+import java.util.Arrays;
+
+/**
+ * @author Wu
+ * @date 2023年07月16日 13:10
+ */
+public class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        int ans = 0;
+        Arrays.sort(nums);
+        int l = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] - nums[l] > k * 2) {
+                l++;
+            }
+            ans = Math.max(ans, i - l + 1);
+        }
+
+        return ans;
+    }
+
+}
---
+++ LeetCode/周赛总/单周塞/周赛354/题3/Solution.java
@@ -0,0 +1,36 @@
+package LeetCode.周赛总.单周塞.周赛354.题3;
+
+import java.util.*;
+
+/**
+ * @author Wu
+ * @date 2023年07月16日 13:10
+ */
+public class Solution {
+    Map<Integer, Integer> cnt = new HashMap<>();
+
+
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
+        for (Integer num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+        int max = 0;
+        int val = -1;
+        for (Map.Entry<Integer, Integer> e : cnt.entrySet()) {
+            if (e.getValue() > max) {
+                val = e.getKey();
+                max = e.getValue();
+            }
+        }
+
+        int c = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums.get(i);
+            if (num == val) c++;
+            if (c * 2 > i + 1 && (max - c) * 2 > (n - i - 1)) return i;
+        }
+
+        return -1;
+    }
+}
