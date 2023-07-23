5dbf1028d4b3e2e33523460e185ea5ea53ffcf69
Jackson_Wu <dengwu.wu@foxmail.com>
2023-07-23 16:25:51 +0800

    feat: 周赛355

---
+++ LeetCode/周赛总/单周塞/周赛355/题1/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode.周赛总.单周塞.周赛355.题1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Wu
+ * @date 2023年07月23日 15:33
+ */
+public class Solution {
+
+    public static List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> ans = new ArrayList<>();
+        for (String word : words) {
+            int idx = 0;
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == separator) {
+                    if (!word.substring(idx, i).equals(""))
+                        ans.add(word.substring(idx, i));
+                    idx = i + 1;
+                }
+            }
+            if (idx < word.length()) ans.add(word.substring(idx));
+        }
+        return ans;
+    }
+}
---
+++ LeetCode/周赛总/单周塞/周赛355/题4/Solution.java
@@ -0,0 +1,9 @@
+package LeetCode.周赛总.单周塞.周赛355.题4;
+
+/**
+ * @author Wu
+ * @date 2023年07月23日 15:33
+ */
+// TODO https://leetcode.cn/problems/count-paths-that-can-form-a-palindrome-in-a-tree/solutions/2355288/yong-wei-yun-suan-chu-li-by-endlesscheng-n9ws/
+public class Solution {
+}
